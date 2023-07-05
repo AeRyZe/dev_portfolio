dcc13d65c0c363a1b6f42a5b83aaa255f98b8284
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 23:10:08 +0200

    feat(banner): added banner base

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,40 @@
+import './banner.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import Galaxy from '../../assets/galaxy.png'
+
+function Banner() {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+    const palette = {
+        white: '#ffffff',
+        red: '#ff6464',
+        yellow: '#fffb80',
+        blue: '#76d5fe'
+    }
+
+    return (
+        <div className='banner-container' id='banner-container'>
+            <h1 data-aos='zoom-in' data-aos-delay='400' data-aos-duration='1000'>Bonjour ! <br/> Je suis Laurent Pardo, développeur front-end et back-end.</h1>
+            <div className='banner-ui'>
+                <FontAwesomeIcon id='banner-star-1' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='banner-star-2' icon={faCircle} beatFade style={{ color: palette.red }} />
+                <FontAwesomeIcon id='banner-star-3' icon={faCircle} beatFade style={{ color: palette.yellow }} />
+                <FontAwesomeIcon id='banner-star-4' icon={faCircle} beatFade style={{ color: palette.blue }} />
+                <FontAwesomeIcon id='banner-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='banner-star-6' icon={faCircle} beatFade style={{ color: palette.red }} />
+                <FontAwesomeIcon id='banner-star-7' icon={faCircle} beatFade style={{ color: palette.yellow }} />
+                <FontAwesomeIcon id='banner-star-8' icon={faCircle} beatFade style={{ color: palette.blue }} />
+                <FontAwesomeIcon id='banner-star-9' icon={faCircle} beatFade style={{ color: palette.white }} />
+            </div>
+            <img className='galaxy' src={Galaxy} alt="Icône d'une galaxie" />
+        </div>
+    )
+}
+
+export default Banner

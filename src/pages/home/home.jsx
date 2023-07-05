1f79cba93f9dc9af44ee93af094508da847ff1cd
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 20:57:52 +0200

    feat(home): added homepage

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import './home.css'
+import React from 'react'
+import About from '../../components/about/about'
+
+function Home() {
+    return (
+        <React.StrictMode>
+            <About />
+        </React.StrictMode>
+    )
+}
+
+export default Home

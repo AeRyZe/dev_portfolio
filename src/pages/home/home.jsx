6836b43b866e44f3b226f2904c28cb43864922c2
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 23:25:27 +0200

    feat(home): added banner comp

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,12 @@
 import './home.css'
 import React from 'react'
+import Banner from '../../components/banner/banner'
 import About from '../../components/about/about'
 
 function Home() {
     return (
         <React.StrictMode>
+            <Banner />
             <About />
         </React.StrictMode>
     )

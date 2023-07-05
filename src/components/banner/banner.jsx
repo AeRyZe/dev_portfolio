e79b9811d15c4a4e591760a95a326ccd227c2fbb
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-05 23:32:08 +0200

    feat(banner): new star shape

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -3,7 +3,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faStarOfLife } from '@fortawesome/free-solid-svg-icons'
 import Galaxy from '../../assets/galaxy.png'
 
 function Banner() {
@@ -22,15 +22,16 @@ function Banner() {
         <div className='banner-container' id='banner-container'>
             <h1 data-aos='zoom-in' data-aos-delay='400' data-aos-duration='1000'>Bonjour ! <br/> Je suis Laurent Pardo, développeur front-end et back-end.</h1>
             <div className='banner-ui'>
-                <FontAwesomeIcon id='banner-star-1' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='banner-star-1' icon={faStarOfLife} beatFade style={{ color: palette.white }} />
                 <FontAwesomeIcon id='banner-star-2' icon={faCircle} beatFade style={{ color: palette.red }} />
                 <FontAwesomeIcon id='banner-star-3' icon={faCircle} beatFade style={{ color: palette.yellow }} />
-                <FontAwesomeIcon id='banner-star-4' icon={faCircle} beatFade style={{ color: palette.blue }} />
+                <FontAwesomeIcon id='banner-star-4' icon={faStarOfLife} beatFade style={{ color: palette.blue }} />
                 <FontAwesomeIcon id='banner-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
                 <FontAwesomeIcon id='banner-star-6' icon={faCircle} beatFade style={{ color: palette.red }} />
-                <FontAwesomeIcon id='banner-star-7' icon={faCircle} beatFade style={{ color: palette.yellow }} />
+                <FontAwesomeIcon id='banner-star-7' icon={faStarOfLife} beatFade style={{ color: palette.yellow }} />
                 <FontAwesomeIcon id='banner-star-8' icon={faCircle} beatFade style={{ color: palette.blue }} />
                 <FontAwesomeIcon id='banner-star-9' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='banner-star-10' icon={faStarOfLife} beatFade style={{ color: palette.red }} />
             </div>
             <img className='galaxy' src={Galaxy} alt="Icône d'une galaxie" />
         </div>

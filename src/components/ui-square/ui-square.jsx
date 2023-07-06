d618ac945d0b21facac60781fc83c7e4fae74c74
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-06 03:51:21 +0200

    feat(ui-square): updated star icon

--- src/components/ui-square/ui-square.jsx
+++ src/components/ui-square/ui-square.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCircle, faStarOfLife } from '@fortawesome/free-solid-svg-icons'
 import Sun from '../../assets/sun.png'
 import Satellite from '../../assets/satellite.png'
 
@@ -22,11 +22,11 @@ function UiSquare(props) {
     if (props.id === '1') {
         return (
             <div className='ui-square-container' data-aos={props.anim} data-aos-anchor={props.anchor} data-aos-delay={props.delay}>
-                <FontAwesomeIcon id='about-star-1' icon={faCircle} beatFade style={{ color: palette.red }} />
-                <FontAwesomeIcon id='about-star-2' icon={faCircle} beatFade style={{ color: palette.blue }} />
+                <FontAwesomeIcon id='about-star-1' icon={faStarOfLife} beatFade style={{ color: palette.red }} />
+                <FontAwesomeIcon id='about-star-2' icon={faCircle} beatFade style={{ color: palette.white }} />
                 <FontAwesomeIcon id='about-star-3' icon={faCircle} beatFade style={{ color: palette.yellow }} />
-                <FontAwesomeIcon id='about-star-4' icon={faCircle} beatFade style={{ color: palette.white }} />
-                <FontAwesomeIcon id='about-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='about-star-4' icon={faStarOfLife} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='about-star-5' icon={faStarOfLife} beatFade style={{ color: palette.blue }} />
                 <FontAwesomeIcon id='about-star-6' icon={faCircle} beatFade style={{ color: palette.red}} />
                 <img id='sun' src={Sun} alt="Icône du Soleil" />
                 <img id='satellite' src={Satellite} alt="Icône d'un satellite" />

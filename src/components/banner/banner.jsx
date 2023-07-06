e61b2e7cc16332575a35985da2abd0553d3b0b0b
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-06 03:50:52 +0200

    feat(banner): added new uis to banner + style

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -5,6 +5,9 @@ import { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle, faStarOfLife } from '@fortawesome/free-solid-svg-icons'
 import Galaxy from '../../assets/galaxy.png'
+import Earth from '../../assets/earth.png'
+import Shuttle from '../../assets/space-shuttle.png'
+import Astronaut from '../../assets/astronaut.png'
 
 function Banner() {
     useEffect(() => {
@@ -21,19 +24,20 @@ function Banner() {
     return (
         <div className='banner-container' id='banner-container'>
             <h1 data-aos='zoom-in' data-aos-delay='400' data-aos-duration='1000'>Bonjour ! <br/> Je suis Laurent Pardo, développeur front-end et back-end.</h1>
-            <div className='banner-ui'>
-                <FontAwesomeIcon id='banner-star-1' icon={faStarOfLife} beatFade style={{ color: palette.white }} />
-                <FontAwesomeIcon id='banner-star-2' icon={faCircle} beatFade style={{ color: palette.red }} />
-                <FontAwesomeIcon id='banner-star-3' icon={faCircle} beatFade style={{ color: palette.yellow }} />
-                <FontAwesomeIcon id='banner-star-4' icon={faStarOfLife} beatFade style={{ color: palette.blue }} />
-                <FontAwesomeIcon id='banner-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
-                <FontAwesomeIcon id='banner-star-6' icon={faCircle} beatFade style={{ color: palette.red }} />
-                <FontAwesomeIcon id='banner-star-7' icon={faStarOfLife} beatFade style={{ color: palette.yellow }} />
-                <FontAwesomeIcon id='banner-star-8' icon={faCircle} beatFade style={{ color: palette.blue }} />
-                <FontAwesomeIcon id='banner-star-9' icon={faCircle} beatFade style={{ color: palette.white }} />
-                <FontAwesomeIcon id='banner-star-10' icon={faStarOfLife} beatFade style={{ color: palette.red }} />
-            </div>
-            <img className='galaxy' src={Galaxy} alt="Icône d'une galaxie" />
+            <FontAwesomeIcon id='banner-star-1' icon={faStarOfLife} beatFade style={{ color: palette.white }} />
+            <FontAwesomeIcon id='banner-star-2' icon={faCircle} beatFade style={{ color: palette.red }} />
+            <FontAwesomeIcon id='banner-star-3' icon={faCircle} beatFade style={{ color: palette.yellow }} />
+            <FontAwesomeIcon id='banner-star-4' icon={faStarOfLife} beatFade style={{ color: palette.blue }} />
+            <FontAwesomeIcon id='banner-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
+            <FontAwesomeIcon id='banner-star-6' icon={faCircle} beatFade style={{ color: palette.red }} />
+            <FontAwesomeIcon id='banner-star-7' icon={faStarOfLife} beatFade style={{ color: palette.yellow }} />
+            <FontAwesomeIcon id='banner-star-8' icon={faCircle} beatFade style={{ color: palette.blue }} />
+            <FontAwesomeIcon id='banner-star-9' icon={faCircle} beatFade style={{ color: palette.white }} />
+            <FontAwesomeIcon id='banner-star-10' icon={faStarOfLife} beatFade style={{ color: palette.red }} />
+            <img id='galaxy' src={Galaxy} alt="Icône d'une galaxie" />
+            <img id='earth' src={Earth} alt="Icône de la Terre" />
+            <img id='shuttle' src={Shuttle} alt="Icône d'une navette spaciale" />
+            <img id='astronaut' src={Astronaut} alt="Icône d'un astronaute" />
         </div>
     )
 }

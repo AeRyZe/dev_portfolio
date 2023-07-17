cd71d28926f48c99b37b221ed4156f8f14f9c938
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-17 02:58:09 +0200

    feat(services): base services integration

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import UiSquare from '../ui-square/ui-square'
+import ServiceCard from '../service-card/service-card'
 
 function Services() {
     useEffect(() => {
@@ -11,8 +12,20 @@ function Services() {
 
     return (
         <div id='services-container'>
-            <h2 id='services-title' data-aos='fade-down' data-aos-anchor='#services-container'>Services</h2>
-            <UiSquare id='2' anim='fade-right' anchor='#services-container' delay='200' />
+            <h2 id='services-title' data-aos='fade-right' data-aos-anchor='#services-container'>Services</h2>
+            <div id='services-content'>
+                <UiSquare id='2' anim='fade-right' anchor='#services-container' delay='200' />
+                <div id='services-cards-container'>
+                    <div id='front-end-cards'>
+                        <h3 data-aos='fade-left' data-aos-anchor='#services-container' data-aos-delay='400'>Front-end</h3>
+                        <ServiceCard icon='' anim='fade-down' anchor='#services-container' delay='600' />
+                    </div>
+                    <div id='back-end-cards'>
+                        <h3 data-aos='fade-left' data-aos-anchor='#services-container' data-aos-delay='800'>Back-end</h3>
+                        <ServiceCard icon='' anim='fade-up' anchor='#services-container' delay='1000' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

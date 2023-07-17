6994176699305ba3bde225c2d132561a93c26bb6
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-18 00:14:12 +0200

    feat(skills-card): base skill card with skill icon

--- src/components/skills-cards/skills-card.jsx
+++ src/components/skills-cards/skills-card.jsx
@@ -1,11 +1,19 @@
 import './skills-card.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-function ServiceCard(props) {
+function SkillsCard(props) {
+    useEffect(() => {
+        AOS.init();
+    })
+
     return (
-        <div className='card-container'>
-
+        <div className='card-container' data-aos={props.anim} data-aos-anchor={props.anchor} data-aos-delay={props.delay}>
+            <FontAwesomeIcon className='skill-card-icon' icon={props.icon} style={{ color: '#ffffff'}} />
         </div>
     )
 }
 
-export default ServiceCard
+export default SkillsCard

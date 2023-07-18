7830a01f73007d79a5d5cda27248f82489005884
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-18 15:23:08 +0200

    feat(hader): anchor links now working

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,9 +8,9 @@ function Header() {
             <nav>
                 <img src={Logo} alt='Logo de Laurent Pardo' className='header-logo' />
                 <div>
-                    <Link to='/' className='header-link'>Accueil</Link>
-                    <Link to='/' className='header-link'>À propos</Link>
-                    <Link to='/' className='header-link'>Compétences</Link>
+                    <Link to='/#banner-container' className='header-link'>Accueil</Link>
+                    <Link to='/#about-container' className='header-link'>À propos</Link>
+                    <Link to='/#skills-container' className='header-link'>Compétences</Link>
                     <Link to='/' className='header-link'>Travaux</Link>
                     <Link to='/' className='header-link'>Contact</Link>
                 </div>

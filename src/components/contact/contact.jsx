2dd6ad274ce56cbf0f0a750931d66a550aa3d290
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-21 19:48:26 +0200

    fix(contact): fixed required

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,9 +6,9 @@ function Contact() {
             <h2 id='contact-title'>Contact</h2>
             <form action="post" id='contact-form'>
                 <label htmlFor="contact-name">Nom</label>
-                <input type="text" name="contact-name" id='contact-name' placeholder='Votre nom et prénom' required='required' />
+                <input type="text" name="contact-name" id='contact-name' placeholder='Votre nom et prénom' required />
                 <label htmlFor="contact-mail">Email</label>
-                <input type="text" name="contact-mail" id='contact-mail' placeholder='Votre adresse mail' required='required' />
+                <input type="text" name="contact-mail" id='contact-mail' placeholder='Votre adresse mail' required />
                 <label htmlFor="contact-message">Message</label>
                 <textarea name="contact-message" id="contact-message" cols="30" rows="15" placeholder='En quoi puis-je vous aider ?' required></textarea>
                 <input type="submit" text='Envoyer' id='contact-send' />

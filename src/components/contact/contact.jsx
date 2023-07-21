a4858046f5a4c5da55cc1d9a1fe3f7fc7cdf72f6
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-21 20:36:08 +0200

    feat(contact): form now sends emails on submit

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,27 @@
 import './contact.css'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_mvbnt7e', 'template_l0jmgqs', form.current, '46K0f3sT8kgeFp-zt')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            
+        e.target.reset();
+    };
+
     return (
         <div id='contact-container'>
             <h2 id='contact-title'>Contact</h2>
-            <form action="post" id='contact-form'>
+            <form ref={form} onSubmit={sendEmail} id='contact-form'>
                 <label htmlFor="contact-name">Nom</label>
                 <input type="text" name="contact-name" id='contact-name' placeholder='Votre nom et prénom' required />
                 <label htmlFor="contact-mail">Email</label>

7c69a43353a1d882e408e9a24f44e4f1a0954364
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-22 02:52:40 +0200

    feat(about): about informations

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -17,8 +17,15 @@ function About() {
             <h2 id='about-title' data-aos='fade-left' data-aos-anchor='#about-container'>À propos</h2>
             <div id='about-content'>
                 <p id='about-text' data-aos='fade-right' data-aos-anchor='#about-container' data-aos-delay='200'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam rerum assumenda deserunt natus omnis vero fugit velit voluptatibus ut culpa, amet quis perspiciatis quam quo iure reprehenderit est autem facere.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam recusandae alias voluptates minus dolorem quaerat animi! Quibusdam quidem, perspiciatis labore autem iste culpa deleniti! Mollitia ad error id architecto laboriosam?
+                    Jeune développeur de 23 ans, je suis passioné de codage depuis plus de 11 ans.
+                    <br></br>
+                    J'ai développé des jeux vidéos sur une plateforme en ligne dénommé ROBLOX.
+                    <br></br>
+                    Ma passion ne faiblissant pas au fil des années, j'ai pris la décision de faire de cette passion mon métier à plein temps à travers le développement web.
+                    <br></br>
+                    C'est ainsi que j'ai décidé de suivre la formation distancielle d'Openclassrooms, formation qui nécessite de travailler en parfaite autonomie et d'être rigoureux.
+                    <br></br>
+                    Je suis quelqu'un de créatif, curieux, flexible, communicatif, rigoureux et très déterminé à apprendre et maîtriser de nouveaux langages/outils.
                 </p>
                 <UiSquare id='1' anim='fade-left' anchor='#about-container' delay='400' />
             </div>

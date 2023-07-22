2d7597c4b39c29c1b23a5853b68a95b25951b95d
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-22 02:08:40 +0200

    feat(ui-square): contact section ui

--- src/components/ui-square/ui-square.jsx
+++ src/components/ui-square/ui-square.jsx
@@ -8,6 +8,9 @@ import Sun from '../../assets/sun.png'
 import Satellite from '../../assets/satellite.png'
 import BlackHole from '../../assets/black-hole.png'
 import Vortex from '../../assets/vortex.png'
+import Mars from '../../assets/mars.png'
+import Uranus from '../../assets/uranus.png'
+import UFO from '../../assets/ufo.png'
 
 function UiSquare(props) {
     const palette = {
@@ -50,7 +53,15 @@ function UiSquare(props) {
     } else if (props.id === '3') {
         return(
             <div className='ui-square-container' data-aos={props.anim} data-aos-anchor={props.anchor} data-aos-delay={props.delay}>
-                
+                <FontAwesomeIcon id='contact-star-1' icon={faStarOfLife} beatFade style={{ color: palette.yellow }} />
+                <FontAwesomeIcon id='contact-star-2' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='contact-star-3' icon={faCircle} beatFade style={{ color: palette.red }} />
+                <FontAwesomeIcon id='contact-star-4' icon={faStarOfLife} beatFade style={{ color: palette.blue }} />
+                <FontAwesomeIcon id='contact-star-5' icon={faCircle} beatFade style={{ color: palette.white }} />
+                <FontAwesomeIcon id='contact-star-6' icon={faCircle} beatFade style={{ color: palette.yellow }} />
+                <img id='mars' src={Mars} alt="Icône de Mars" />
+                <img id='ufo' src={UFO} alt="Icône d'un alien dans un UFO" />
+                <img id='uranus' src={Uranus} alt="" />
             </div>
         )
     }

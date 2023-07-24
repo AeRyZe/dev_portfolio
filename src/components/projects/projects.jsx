f19c1c56d56ffd40f041608b502c5d005114dcfc
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-24 16:02:09 +0200

    feat(projects): displaying projects from json

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,15 +1,21 @@
 import './projects.css'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import ProjectCard from '../project-card/project-card'
-import Booki from '../../assets/projects/booki.png'
-import SophieBluel from '../../assets/projects/sophiebluel.png'
-import NinaCarducci from '../../assets/projects/ninacarducci.png'
-import Kasa from '../../assets/projects/kasa.png'
-import MonVieuxGrimoire from '../../assets/projects/monvieuxgrimoire.png'
 
 function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        async function getData() {
+            const results = await fetch('/projects.json');
+            const data = await results.json();
+            setProjects(data);
+        }
+        getData();
+    }, []);
+
     useEffect(() => {
         AOS.init();
     });
@@ -18,56 +24,23 @@ function Projects() {
         <div id='projects-container'>
             <h2 id='projects-title' data-aos='fade-left' data-aos-anchor='#projects-container'>Travaux</h2>
             <div id='projects-content'>
-                <ProjectCard
-                    title='Booki'
-                    image={Booki}
-                    link='https://aeryze.github.io/Booki/'
-                    text="créer la page d'accueil d'une agence de voyage avec HTML & CSS ; rendre un site web responsive pour appareils mobiles"
-                    code='https://github.com/AeRyZe/Booki'
-                    anim='fade-right'
-                    anchor='#projects-container'
-                    delay='200'
-                />
-                <ProjectCard
-                    title='Sophie Bluel'
-                    image={SophieBluel}
-                    link='https://aeryze.github.io/SophieBluel/FrontEnd'
-                    text="créer une page web dynamique avec JavaScript ; afficher des données à l'aide d'une API"
-                    code='https://github.com/AeRyZe/SophieBluel'
-                    anim='fade-left'
-                    anchor='#projects-container'
-                    delay='300'
-                />
-                <ProjectCard
-                    title='Nina Carducci'
-                    image={NinaCarducci}
-                    link='https://aeryze.github.io/nina-carducci.github.io/'
-                    text="débugger et optimiser un site de photographe (optimisation des images, optimisation de la structure du code, accroître le référencement)"
-                    code='https://github.com/AeRyZe/nina-carducci.github.io'
-                    anim='fade-right'
-                    anchor='#projects-container'
-                    delay='400'
-                />
-                <ProjectCard
-                    title='Kasa'
-                    image={Kasa}
-                    link='https://kasa-self.vercel.app/'
-                    text="créer une application web de location immobilière avec ReactJS ; utilisation de SASS pour améliorer l'interface"
-                    code='https://github.com/AeRyZe/kasa'
-                    anim='fade-left'
-                    anchor='#projects-container'
-                    delay='500'
-                />
-                <ProjectCard
-                    title='Mon Vieux Grimoire'
-                    link='https://mon-vieux-grimoire-server.onrender.com/api/books'
-                    image={MonVieuxGrimoire}
-                    text="développer le back-end d'un site de notation de livres avec NodeJS ; application des bonnes pratiques Green Code"
-                    code='https://github.com/AeRyZe/mon-vieux-grimoire'
-                    anim='fade-up'
-                    anchor='#projects-container'
-                    delay='600'
-                />
+                {
+                    projects.map((project, index) => {
+                        return (
+                            <ProjectCard
+                                key={project.name}
+                                title={project.name}
+                                image={project.image}
+                                link={project.link}
+                                text={project.mission}
+                                code={project.code}
+                                anim={index % 2 ? 'fade-left' : 'fade-right'}
+                                anchor='#projects-container'
+                                delay={`${200 * index}`}
+                            />
+                        )
+                    })
+                }
             </div>
         </div>
     )

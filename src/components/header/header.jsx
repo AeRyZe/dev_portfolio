ca1ced7638e812c6bebdb1071549097a88942991
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2023-07-29 20:32:12 +0200

    feat(header): smooth scroll to anchor

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,7 @@
 import './header.css'
 import { useState } from 'react'
 import Logo from '../../assets/wlogo.png'
-import { HashLink as Link } from 'react-router-hash-link'
+import { Link } from "react-scroll";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 
@@ -17,21 +17,21 @@ function Header() {
             <nav>
                 <img src={Logo} alt='Logo de Laurent Pardo' className='header-logo' />
                 <div id='header-links'>
-                    <Link to='/#banner-container' className='header-link'>Accueil</Link>
-                    <Link to='/#about-container' className='header-link'>À propos</Link>
-                    <Link to='/#skills-container' className='header-link'>Compétences</Link>
-                    <Link to='/#projects-container' className='header-link'>Travaux</Link>
-                    <Link to='/#contact-container' className='header-link'>Contact</Link>
+                    <Link to='banner-container' smooth={true} className='header-link'>Accueil</Link>
+                    <Link to='about-container' smooth={true} className='header-link'>À propos</Link>
+                    <Link to='skills-container' smooth={true} className='header-link'>Compétences</Link>
+                    <Link to='projects-container' smooth={true} className='header-link'>Travaux</Link>
+                    <Link to='contact-container' smooth={true} className='header-link'>Contact</Link>
                 </div>
                 <div id='menu-container' onClick={toggle}>
                     <FontAwesomeIcon id='menu-button' icon={isOpen ? faClose : faBars} />
                 </div>
                 <div id={isOpen ? 'menu-open' : 'menu-close'}>
-                    <Link to='/#banner-container' className='menu-link'>Accueil</Link>
-                    <Link to='/#about-container' className='menu-link'>À propos</Link>
-                    <Link to='/#skills-container' className='menu-link'>Compétences</Link>
-                    <Link to='/#projects-container' className='menu-link'>Travaux</Link>
-                    <Link to='/#contact-container' className='menu-link'>Contact</Link>
+                    <Link to='banner-container' smooth={true} className='menu-link'>Accueil</Link>
+                    <Link to='about-container' smooth={true} className='menu-link'>À propos</Link>
+                    <Link to='skills-container' smooth={true} className='menu-link'>Compétences</Link>
+                    <Link to='projects-container' smooth={true} className='menu-link'>Travaux</Link>
+                    <Link to='contact-container' smooth={true} className='menu-link'>Contact</Link>
                 </div>
             </nav>
         </header>

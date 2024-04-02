c167694783837c0141d5a27d2fe24faa83a9b9db
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2024-04-02 06:48:11 +0200

    feat(home): gsap & tsParticles setups

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,19 +1,64 @@
 import './home.css'
-import React from 'react'
-import Banner from '../../components/banner/banner'
-import About from '../../components/about/about'
-import Skills from '../../components/skills/skills'
-import Projects from '../../components/projects/projects'
-import Contact from '../../components/contact/contact'
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from '@tsparticles/slim';
+//import { loadAll } from '@tsparticles/all';
+//import { loadFull } from 'tsparticles';
+import Banner from '../../components/banner/banner';
+import About from '../../components/about/about';
+import Skills from '../../components/skills/skills';
+import Projects from '../../components/projects/projects';
+import Contact from '../../components/contact/contact';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Home() {
+    useEffect(() => {
+        // GSAP
+        let separators = document.querySelectorAll('.separator');
+        separators.forEach((that) => {
+            gsap.fromTo(that, {
+                width: 0,
+                left: "47.5vw"
+            }, {
+                width: "30vw",
+                left: "35vw",
+                duration: 0.5,
+                scrollTrigger: {
+                    trigger: that,
+                    start: "bottom bottom",
+                    toggleActions: "play none none reverse"
+                }
+            });
+        });
+
+        // tsParticles
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // ex: await loadHyperspacePreset(engine);
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine);
+            //await loadBasic(engine);
+        });
+    }, []);
+
     return (
         <main>
-            <Banner />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
+            <React.StrictMode>
+                <Banner />
+                <About />
+                <div className='separator'></div>
+                <Skills />
+                <div className='separator'></div>
+                <Projects />
+                <div className='separator'></div>
+                <Contact />
+            </React.StrictMode>
         </main>
     )
 }

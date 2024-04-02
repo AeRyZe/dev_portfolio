58c78f70a299304bd8118084d972843e98796bc6
ÆRyZe <118224947+AeRyZe@users.noreply.github.com>
2024-04-02 06:47:01 +0200

    feat(skills): added new skills & dynamic sliders

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -4,8 +4,8 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import UiSquare from '../ui-square/ui-square'
 import SkillsCard from '../skill-card/skill-card'
-import { faHtml5, faSquareJs, faReact, faNode, faPhp } from '@fortawesome/free-brands-svg-icons'
-import { faLeaf } from '@fortawesome/free-solid-svg-icons'
+import { faHtml5, faCss3Alt, faSass, faSquareJs, faReact, faNode, faPhp } from '@fortawesome/free-brands-svg-icons'
+import { faLeaf, faDatabase } from '@fortawesome/free-solid-svg-icons'
 
 function Skills() {
     useEffect(() => {
@@ -20,20 +20,36 @@ function Skills() {
                 <div id='skills-cards-container'>
                     <h3 data-aos='fade-left' data-aos-anchor='#skills-container' data-aos-delay={window.innerWidth <= 993 ? '200' : '400'}>Front-end</h3>
                     <div id='front-end-cards'>
-                        <SkillsCard icon={faHtml5} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='HTML5 & CSS3' />
-                        <SkillsCard icon={faSquareJs} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='JavaScript' />
-                        <SkillsCard icon={faReact} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='ReactJS' />
+                        <div id='front-slider'>
+                            <SkillsCard icon={faHtml5} width='5vw' anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='HTML5' />
+                            <SkillsCard icon={faCss3Alt} width='5vw' anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='CSS3' />
+                            <SkillsCard icon={faSass} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='SASS' />
+                            <SkillsCard icon={faSquareJs} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='JavaScript' />
+                            <SkillsCard icon={faReact} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='ReactJS' />
+                            <SkillsCard icon={faHtml5} width='5vw' anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='HTML5' />
+                            <SkillsCard icon={faCss3Alt} width='5vw' anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='CSS3' />
+                            <SkillsCard icon={faSass} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='SASS' />
+                            <SkillsCard icon={faSquareJs} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='JavaScript' />
+                            <SkillsCard icon={faReact} anim='fade-down' anchor='#skills-container' delay={window.innerWidth <= 993 ? '400' : '600'} text='ReactJS' />
+                        </div>
                     </div>
                     <h3 data-aos='fade-left' data-aos-anchor='#skills-container' data-aos-delay={window.innerWidth <= 993 ? '600' : '800'}>Back-end</h3>
                     <div id='back-end-cards'>
-                        <SkillsCard icon={faNode} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='NodeJS' />
-                        <SkillsCard icon={faLeaf} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='MongoDB' />
-                        <SkillsCard icon={faPhp} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='PHP' />
+                        <div id='back-slider'>
+                            <SkillsCard icon={faNode} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='NodeJS' />
+                            <SkillsCard icon={faLeaf} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='MongoDB' />
+                            <SkillsCard icon={faPhp} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='PHP' />
+                            <SkillsCard icon={faDatabase} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='MariaDB' />
+                            <SkillsCard icon={faNode} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='NodeJS' />
+                            <SkillsCard icon={faLeaf} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='MongoDB' />
+                            <SkillsCard icon={faPhp} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='PHP' />
+                            <SkillsCard icon={faDatabase} anim='fade-up' anchor='#skills-container' delay={window.innerWidth <= 993 ? '800' : '1000'} text='MariaDB' />
+                        </div>
                     </div>
                 </div>
             </div>
             <p id='skills-tools' data-aos='fade-up' data-aos-anchor='#skills-container' data-aos-delay={'1200'}>
-                <b>Outils :</b> GitHub, Visual Studio Code, npm, SASS, Postman, PhpMyAdmin, WAMP, Lighthouse, Google Analytics
+                <b>Outils :</b> GitHub, Visual Studio Code, npm, Postman, PhpMyAdmin, WAMP, Lighthouse, Google Analytics
             </p>
         </section>
     )
